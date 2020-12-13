e371df3ed9c64b693d3cd6e01c19c3f0277a7faf
raquelvilas18 <38087497+raquelvilas18@users.noreply.github.com>
2020-12-13 15:43:23 +0100

    Files organization ui folder Refactor names of activitys

--- app/src/main/java/com/example/kedi/ui/PetfundingActivity.kt
+++ app/src/main/java/com/example/kedi/ui/PetfundingActivity.kt
@@ -1,7 +1,8 @@
-package com.example.kedi
+package com.example.kedi.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.kedi.R
 
 class PetfundingActivity : AppCompatActivity() {
 
